9e81b0c9184f58cecefe15a73319c5b3648395f5
albertoaldape18 <albertoaldape18@gmail.com>
2018-10-06 01:51:03 -0500

    Practica 4

---
+++ Practica4-Almendarez.sql
@@ -0,0 +1,114 @@
+--LBD Juan Alberto Almendarez Aldape
+
+use Punto_venta
+
+SELECT * FROM Proveedor_E
+--10
+insert into Proveedor_E(id_proveedor, RFC, n_comercial, n_oficial, id_direccion, id_insumo, id_telefono) values (249631, 23356,'Pepsi','657', 56800, 45300, 65400)
+insert into Proveedor_E(id_proveedor, RFC, n_comercial, n_oficial, id_direccion, id_insumo, id_telefono) values (781125, 11452,'Coca-Cola','658', 56701, 45401, 65501)
+insert into Proveedor_E(id_proveedor, RFC, n_comercial, n_oficial, id_direccion, id_insumo, id_telefono) values (785998, 45577,'Gamesa','659', 86771, 45771, 67800)
+insert into Proveedor_E(id_proveedor, RFC, n_comercial, n_oficial, id_direccion, id_insumo, id_telefono) values (638021, 77001,'Sabritas','660', 90014, 45899, 01175)
+insert into Proveedor_E(id_proveedor, RFC, n_comercial, n_oficial, id_direccion, id_insumo, id_telefono) values (602287, 53021,'Acer','661', 56870, 12547, 12896)
+insert into Proveedor_E(id_proveedor, RFC, n_comercial, n_oficial, id_direccion, id_insumo, id_telefono) values (120045, 12004,'Apple','662', 48788, 56870, 45899)
+insert into Proveedor_E(id_proveedor, RFC, n_comercial, n_oficial, id_direccion, id_insumo, id_telefono) values (121214, 32133,'LG','663', 77841, 56800, 45300)
+insert into Proveedor_E(id_proveedor, RFC, n_comercial, n_oficial, id_direccion, id_insumo, id_telefono) values (996541, 11258,'Motorola','664', 22392, 77841, 56800)
+insert into Proveedor_E(id_proveedor, RFC, n_comercial, n_oficial, id_direccion, id_insumo, id_telefono) values (859599, 44787,'Purina','665', 11289, 56088, 45033)
+insert into Proveedor_E(id_proveedor, RFC, n_comercial, n_oficial, id_direccion, id_insumo, id_telefono) values (369845, 42565,'LALA','789', 58044, 54704, 95804)
+
+SELECT * FROM direccion_b
+--20
+insert into direccion_b(id_direccion, calle, numero, colonia, municipio, estado, pais) values (56800, 'calle4', 756, 'colonia1', 'municipio1', 'NL', 'MX')
+insert into direccion_b(id_direccion, calle, numero, colonia, municipio, estado, pais) values (56701, 'calle3', 785, 'colonia2', 'municipio2', 'NL', 'MX')
+insert into direccion_b(id_direccion, calle, numero, colonia, municipio, estado, pais) values (86771, 'calle2', 804, 'colonia3', 'municipio3', 'NL', 'MX')
+insert into direccion_b(id_direccion, calle, numero, colonia, municipio, estado, pais) values (90014, 'calle1', 458, 'colonia4', 'municipio4', 'NL', 'MX')
+insert into direccion_b(id_direccion, calle, numero, colonia, municipio, estado, pais) values (56870, 'calle1', 744, 'colonia4', 'municipio3', 'NL', 'MX')
+insert into direccion_b(id_direccion, calle, numero, colonia, municipio, estado, pais) values (48788, 'calle1', 784, 'colonia4', 'municipio4', 'NL', 'MX')
+insert into direccion_b(id_direccion, calle, numero, colonia, municipio, estado, pais) values (77841, 'calle4', 145, 'colonia3', 'municipio1', 'NL', 'MX')
+insert into direccion_b(id_direccion, calle, numero, colonia, municipio, estado, pais) values (22392, 'calle3', 102, 'colonia2', 'municipio2', 'NL', 'MX')
+insert into direccion_b(id_direccion, calle, numero, colonia, municipio, estado, pais) values (11289, 'calle2', 788, 'colonia2', 'municipio3', 'NL', 'MX')
+insert into direccion_b(id_direccion, calle, numero, colonia, municipio, estado, pais) values (58044, 'calle3', 986, 'colonia1', 'municipio3', 'NL', 'MX')
+insert into direccion_b(id_direccion, calle, numero, colonia, municipio, estado, pais) values (48789, 'calle4', 115, 'colonia1', 'municipio2', 'NL', 'MX')
+insert into direccion_b(id_direccion, calle, numero, colonia, municipio, estado, pais) values (77842, 'calle4', 963, 'colonia1', 'municipio1', 'NL', 'MX')
+insert into direccion_b(id_direccion, calle, numero, colonia, municipio, estado, pais) values (22393, 'calle3', 889, 'colonia4', 'municipio3', 'NL', 'MX')
+insert into direccion_b(id_direccion, calle, numero, colonia, municipio, estado, pais) values (11288, 'calle2', 332, 'colonia4', 'municipio1', 'NL', 'MX')
+insert into direccion_b(id_direccion, calle, numero, colonia, municipio, estado, pais) values (58045, 'calle2', 454, 'colonia2', 'municipio2', 'NL', 'MX')
+insert into direccion_b(id_direccion, calle, numero, colonia, municipio, estado, pais) values (58086, 'calle3', 149, 'colonia2', 'municipio4', 'NL', 'MX')
+insert into direccion_b(id_direccion, calle, numero, colonia, municipio, estado, pais) values (60007, 'calle4', 237, 'colonia4', 'municipio4', 'NL', 'MX')
+insert into direccion_b(id_direccion, calle, numero, colonia, municipio, estado, pais) values (61008, 'calle1', 172, 'colonia3', 'municipio1', 'NL', 'MX')
+insert into direccion_b(id_direccion, calle, numero, colonia, municipio, estado, pais) values (70089, 'calle2', 199, 'colonia3', 'municipio2', 'NL', 'MX')
+insert into direccion_b(id_direccion, calle, numero, colonia, municipio, estado, pais) values (61009, 'calle3', 200, 'colonia3', 'municipio1', 'NL', 'MX')
+
+
+
+SELECT * FROM telefono_a
+--15
+insert into telefono_a(id_telefono, num_telefono, zona) values (65400, '8114542112','52')
+insert into telefono_a(id_telefono, num_telefono, zona) values (65501, '8114565894','52')
+insert into telefono_a(id_telefono, num_telefono, zona) values (67800, '8124879665','52')
+insert into telefono_a(id_telefono, num_telefono, zona) values (01175, '8187479858','52')
+insert into telefono_a(id_telefono, num_telefono, zona) values (12896, '8110120356','52')
+insert into telefono_a(id_telefono, num_telefono, zona) values (45899, '8118994563','52')
+insert into telefono_a(id_telefono, num_telefono, zona) values (45300, '8189795785','52')
+insert into telefono_a(id_telefono, num_telefono, zona) values (56800, '8124541010','52')
+insert into telefono_a(id_telefono, num_telefono, zona) values (45033, '8121003663','52')
+insert into telefono_a(id_telefono, num_telefono, zona) values (95804, '8112003698','52')
+insert into telefono_a(id_telefono, num_telefono, zona) values (95906, '8118971002','52')
+insert into telefono_a(id_telefono, num_telefono, zona) values (66067, '8123004568','52')
+insert into telefono_a(id_telefono, num_telefono, zona) values (66108, '8117002001','52')
+insert into telefono_a(id_telefono, num_telefono, zona) values (45601, '8189005050','52')
+insert into telefono_a(id_telefono, num_telefono, zona) values (67108, '8125856321','52')
+
+SELECT * FROM insumo_a
+--15
+insert into insumo_a(id_insumo, id_producto) values (45100, 56800)
+insert into insumo_a(id_insumo, id_producto) values (45200, 56701)
+insert into insumo_a(id_insumo, id_producto) values (45300, 86771)
+insert into insumo_a(id_insumo, id_producto) values (45400, 90014)
+insert into insumo_a(id_insumo, id_producto) values (45500, 56870)
+insert into insumo_a(id_insumo, id_producto) values (45600, 48788)
+insert into insumo_a(id_insumo, id_producto) values (45700, 77841)
+insert into insumo_a(id_insumo, id_producto) values (45800, 22392)
+insert into insumo_a(id_insumo, id_producto) values (45900, 11289)
+insert into insumo_a(id_insumo, id_producto) values (46100, 58044)
+insert into insumo_a(id_insumo, id_producto) values (46200, 59085)
+insert into insumo_a(id_insumo, id_producto) values (46300, 60006)
+insert into insumo_a(id_insumo, id_producto) values (46400, 61007)
+insert into insumo_a(id_insumo, id_producto) values (46500, 70088)
+insert into insumo_a(id_insumo, id_producto) values (46600, 61007)
+
+
+SELECT * FROM Empleado_C
+--10
+insert into Empleado_C(id_empleado, nombre, foto, edad, estatura, peso, id_direccion, id_telefono, sexo, salario, id_puesto) values (9865188, 'Sebastian Suarez', null, 36, 1.81, 85, 48789, 45890, 0, 25000, 36971)
+insert into Empleado_C(id_empleado, nombre, foto, edad, estatura, peso, id_direccion, id_telefono, sexo, salario, id_puesto) values (7621562, 'Tomás Rodriguez', null, 19, 1.88, 85, 77842, 45301, 0, 35000, 76767)
+insert into Empleado_C(id_empleado, nombre, foto, edad, estatura, peso, id_direccion, id_telefono, sexo, salario, id_puesto) values (7854588, 'Hector Trejo', null, 25, 1.70, 80, 22393, 56801, 0, 25000, 36971)
+insert into Empleado_C(id_empleado, nombre, foto, edad, estatura, peso, id_direccion, id_telefono, sexo, salario, id_puesto) values (1458544, 'Jennifer Gonzalez', null, 32, 1.71, 85, 11288, 45034, 1, 15000, 45698)
+insert into Empleado_C(id_empleado, nombre, foto, edad, estatura, peso, id_direccion, id_telefono, sexo, salario, id_puesto) values (1225478, 'Fernando Cepeda', null, 19, 1.80, 65, 58045, 95805, 0, 15000, 45698)
+insert into Empleado_C(id_empleado, nombre, foto, edad, estatura, peso, id_direccion, id_telefono, sexo, salario, id_puesto) values (4125877, 'Teresa Urbina', null, 24, 1.69, 59, 59086, 95906, 1, 10000, 13658)
+insert into Empleado_C(id_empleado, nombre, foto, edad, estatura, peso, id_direccion, id_telefono, sexo, salario, id_puesto) values (3256899, 'Jesus Zavala', null, 22, 1.73, 65, 60007, 66007, 0, 20000, 69654)
+insert into Empleado_C(id_empleado, nombre, foto, edad, estatura, peso, id_direccion, id_telefono, sexo, salario, id_puesto) values (3358788, 'Ricardo Ofarril', null, 31, 1.75, 75, 61008, 66108, 0, 30000, 76767)
+insert into Empleado_C(id_empleado, nombre, foto, edad, estatura, peso, id_direccion, id_telefono, sexo, salario, id_puesto) values (1244787, 'Gonzalo Yado', null, 23, 1.78, 78, 70089, 45601, 0, 21000, 69654)
+insert into Empleado_C(id_empleado, nombre, foto, edad, estatura, peso, id_direccion, id_telefono, sexo, salario, id_puesto) values (8897548, 'Yewen Zaho', null, 21, 1.91, 82, 61009, 67108, 0, 22000, 69654)
+
+SELECT * FROM puesto_b
+--5
+insert into puesto_b(id_puesto, n_puesto, nivel) values (76767, 'Gerente', 3)
+insert into puesto_b(id_puesto, n_puesto, nivel) values (45698, 'Cajero', 2)
+insert into puesto_b(id_puesto, n_puesto, nivel) values (36971, 'Tesorero', 3)
+insert into puesto_b(id_puesto, n_puesto, nivel) values (13658, 'Paquetero', 1)
+insert into puesto_b(id_puesto, n_puesto, nivel) values (69654, 'Administrador', 2)
+
+
+--update									
+update telefono_a set num_telefono='8114565800' where id_telefono = 67108
+update direccion_b set municipio = 'municipio 1'  where id_direccion=56701
+update telefono_a set num_telefono='8128125555' where id_telefono = 66108
+update direccion_b set calle = 'calle1' where id_direccion=61009
+update direccion_b set colonia = 'colonia2' where id_direccion=22393
+
+--delete
+delete from Proveedor_E where id_proveedor = 369845
+delete from Empleado_C where id_empleado= 4125877
+delete from insumo_a where id_insumo=46600
+delete from direccion_b where id_direccion=61009
+delete from insumo_a where id_insumo=46500
